d8f2232b9b20742d8c44a339ab58c058ddfb7682
Marcoo09 <marcofiorito1@gmail.com>
2024-07-28 10:42:41 -0300

    update config file

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,1 +1,31 @@
-const swconf = { cacheName: 'chirpy-1722173377',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', '/assets/js/dist/app.min.js', '/assets/js/dist/categories.min.js', '/assets/js/dist/commons.min.js', '/assets/js/dist/home.min.js', '/assets/js/dist/misc.min.js', '/assets/js/dist/page.min.js', '/assets/js/dist/post.min.js' ],denyPaths: [ ], purge: false };
+const swconf = {
+  
+    cacheName: 'chirpy-1722174141',resources: [
+      '/Marcoo09.github.io/assets/css/jekyll-theme-chirpy.css',
+      '/Marcoo09.github.io/',
+      
+        '/Marcoo09.github.io/categories/',
+      
+        '/Marcoo09.github.io/tags/',
+      
+        '/Marcoo09.github.io/archives/',
+      
+        '/Marcoo09.github.io/about/',
+      
+
+      
+      
+        '/Marcoo09.github.io/assets/js/dist/app.min.js',
+        '/Marcoo09.github.io/assets/js/dist/categories.min.js',
+        '/Marcoo09.github.io/assets/js/dist/commons.min.js',
+        '/Marcoo09.github.io/assets/js/dist/home.min.js',
+        '/Marcoo09.github.io/assets/js/dist/misc.min.js',
+        '/Marcoo09.github.io/assets/js/dist/page.min.js',
+        '/Marcoo09.github.io/assets/js/dist/post.min.js'
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
